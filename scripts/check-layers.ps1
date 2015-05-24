32945df5223d4e15f7aa06203408c15c4fea4485
Kent Stanton <kent.stanton@gmail.com>
2015-05-24 12:47:28 -0400

    CommunityGIS BBD Project Initial Commit
    QGIS project and layer management script. Initial commit.

---
+++ scripts/check-layers.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+   Script - List Base Layers associated with the community GIS project.
+.DESCRIPTION
+   
+.EXAMPLE
+   ./check-layers
+#>
+
+
+$ProjectRootPath = "C:\myworld\CGIS\BBD"
+$ProjectConfigPath = "$ProjectRootPath\config"
+$baselayers = Get-Content -Path "$ProjectConfigPath\baselayers.csv"
+
+
+$parsed = $baselayers | ConvertFrom-csv
+
+$parsed | Out-GridView
